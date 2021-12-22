b938fb920928145d36a6b429a2019ba8b391ce2a
Jose A Izaguirre <joseadolfoizaguirreflores@gmail.com>
2021-12-22 15:41:16 -0600

    LoadFile and SaveFile class functionality implemented

--- src/main/java/file/LoadFile.java
+++ src/main/java/file/LoadFile.java
@@ -1,5 +1,25 @@
 package file;
 
-public class LoadFile {
-    private String load;
+import entities.User;
+
+import java.io.*;
+
+public class LoadFile implements Serializable {
+
+    private File miFile = new File("User.todo");
+    private ObjectInputStream ois;
+    private User Usuario;
+
+    public User Cargar(){
+
+        try {
+            this.ois = new ObjectInputStream(new FileInputStream(this.miFile));
+            this.Usuario = (User) ois.readObject();
+            return this.Usuario;
+        }catch(IOException | ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        return this.Usuario;
+    }
 }
--- src/main/java/file/SaveFile.java
+++ src/main/java/file/SaveFile.java
@@ -1,6 +1,23 @@
 package file;
 
-public class SaveFile {
+import entities.User;
 
-    private String Save;
+import java.io.*;
+
+public class SaveFile implements Serializable {
+
+    private File miFile = new File("User.todo");
+    private ObjectOutputStream oos;
+
+    public void Guardar(User usuario){
+
+        try {
+            this.oos = new ObjectOutputStream(new FileOutputStream(this.miFile));
+            oos.writeObject(usuario);
+            oos.close();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
