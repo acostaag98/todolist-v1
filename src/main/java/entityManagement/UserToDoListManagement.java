d2bae8f59aa5f711f5d63d8e4e854f1fee1b82fa
Samz Mendoza <samzmendoza@gmail.com>
2021-12-22 16:25:10 -0600

    UserToDoListManagment

--- src/main/java/entityManagement/UserToDoListManagement.java
+++ src/main/java/entityManagement/UserToDoListManagement.java
@@ -1,9 +1,28 @@
 package entityManagement;
 
 import entities.User;
+import interfaces.createObject;
+import interfaces.deleteObject;
+import interfaces.updateObject;
 
-public class UserToDoListManagement {
+public class UserToDoListManagement implements createObject, updateObject, deleteObject {
 
     private User user;
+    private int IdToDo;
 
+
+    @Override
+    public void create_Object() {
+
+    }
+
+    @Override
+    public void delete_Object() {
+
+    }
+
+    @Override
+    public void update_Object() {
+
+    }
 }
