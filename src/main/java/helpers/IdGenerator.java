c04bc91476a0f16615a2182a60e63284118e6845
Jose A Izaguirre <joseadolfoizaguirreflores@gmail.com>
2022-01-22 17:48:05 -0600

    Refactoring-> Moving Features Between Objects-> Replace Magic Number with Symbolic Constant

--- src/main/java/helpers/IdGenerator.java
+++ src/main/java/helpers/IdGenerator.java
@@ -1,11 +1,15 @@
 package helpers;
 
 public class IdGenerator {
+
+    //applying Replace Magic Number with Symbolic Constant method
+    static final int INTEGER_CONSTANT = 1;
+
     //applying Introduce Foreign Method
     public String AssignUniqueId(int hash){
         int tmpID = hash;
 
-        if (tmpID < 1){
+        if (tmpID < INTEGER_CONSTANT){
             tmpID = -1 * tmpID;
         }
         return String.valueOf( tmpID );
