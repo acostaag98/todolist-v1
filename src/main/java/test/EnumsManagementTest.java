b055307482432f8b4337878a0256e603ea43f11f
Jose A Izaguirre <joseadolfoizaguirreflores@gmail.com>
2022-01-23 01:00:59 -0600

    EnumsManagement jUnit tests implemented

---
+++ src/main/java/test/EnumsManagementTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import enums.actionType;
+import enums.priorityType;
+import enums.stateType;
+import helpers.EnumsManagement;
+import org.junit.Test;
+
+import java.lang.reflect.AccessibleObject;
+
+import static org.junit.Assert.*;
+
+public class EnumsManagementTest {
+
+
+
+    @Test
+    public void find_PriorityType_By_Wrong_Value() {
+
+        assertEquals(null, new EnumsManagement().findPriorityByValue("José"));
+    }
+
+    @Test
+    public void find_PriorityType_By_Correct_Value() {
+
+        assertEquals( priorityType.LOW, new EnumsManagement().findPriorityByValue("Low"));
+    }
+
+    @Test
+    public void find_StateType_By_Wrong_Value() {
+
+        assertEquals(null, new EnumsManagement().findStateByValue("Malo"));
+    }
+
+    @Test
+    public void find_StateType_By_Correct_Value() {
+
+        assertEquals( stateType.FINISHED, new EnumsManagement().findStateByValue("Finished"));
+    }
+
+}
