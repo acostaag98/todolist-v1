d8d8862f7396ab18a5eb4831c4e9385d768f2295
Jose A Izaguirre <joseadolfoizaguirreflores@gmail.com>
2022-01-22 18:21:02 -0600

    Refactoring-> Making Method Calls Simpler-> Replace Error Code with Exception

--- src/main/java/entityManagement/UserToDoListManagement.java
+++ src/main/java/entityManagement/UserToDoListManagement.java
@@ -16,13 +16,15 @@ public class UserToDoListManagement implements addObject, updateObjectIntoObject
 
     @Override
     public void showDetail(Object user, String toDoID) {
-        int position = searchToDo( (User) user, toDoID);
 
-        if (position < 0){
-            JOptionPane.showMessageDialog(null, "The ToDo doesn't exist", "Error", JOptionPane.ERROR_MESSAGE);
+        try {
+            int position = searchToDo( (User) user, toDoID);
+
+            this.toDoManagement.showDetail( ((User) user).getToDos().get(position) );
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
-
-        this.toDoManagement.showDetail( ((User) user).getToDos().get(position) );
     }
 
 
@@ -40,34 +42,42 @@ public class UserToDoListManagement implements addObject, updateObjectIntoObject
 
     @Override
     public void delete( Object user, String toDoID ) {
-        int position = searchToDo( (User) user, toDoID);
 
-        if (position < 0){
-            JOptionPane.showMessageDialog(null, "The ToDo doesn't exist", "Error", JOptionPane.ERROR_MESSAGE);
+        try {
+            int position = searchToDo( (User) user, toDoID);
+
+            this.toDoManagement.delete( ((User) user).getToDos().get(position) );
+            ((User) user).getToDos().remove(position);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
 
-        this.toDoManagement.delete( ((User) user).getToDos().get(position) );
-        ((User) user).getToDos().remove(position);
     }
 
     @Override
     public void update(Object user, String toDoID) {
-        int position = searchToDo( (User) user, toDoID);
 
-        if (position < 0){
-            JOptionPane.showMessageDialog(null, "The ToDo doesn't exist", "Error", JOptionPane.ERROR_MESSAGE);
+        try {
+            int position = searchToDo( (User) user, toDoID);
+
+            this.toDoManagement.update( ((User) user).getToDos().get(position) );
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
 
-        this.toDoManagement.update( ((User) user).getToDos().get(position) );
     }
 
-    private int searchToDo(User user, String toDoID){
+    private int searchToDo(User user, String toDoID) throws Exception {
+
         for (int i = 0; i < user.getToDos().size(); i++) {
             if ( user.getToDos().get(i).getId() == toDoID ){
                 return i;
             }
         }
-        return -1;
+
+        throw new Exception("The ToDo doesn't exist");
     }
 
 
